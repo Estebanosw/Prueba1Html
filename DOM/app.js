0025c4667744d403134895be715baac4d6ecb714
Esteban Oswaldo Prieto Riaño <oswaldo@appinit.co>
2024-10-07 07:55:16 -0500

    Creación de DOM

---
+++ DOM/app.js
@@ -0,0 +1,23 @@
+// let elemento = document;
+
+// elemento = document.body;
+
+// console.log(elemento);
+
+// let elementoId = document.getElementById('TituloDOM');
+// console.log(elementoId);
+
+// let elementoId1 = document.getElementById('Contenido');
+// console.log(elementoId1);
+
+let opid =document.getElementById('p1');
+opid.innerHTML = 'JavaScript';
+
+let opid2 =document.getElementById('p2');
+opid2.innerHTML = 'Kotlin';
+
+let opid3 =document.getElementById('p3');
+opid3.innerHTML = 'C++';
+
+let elementoclase = document.getElementsByClassName('listas');
+console.log(elementoclase);
